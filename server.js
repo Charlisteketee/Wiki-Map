5b4e19cb90d8f5ff9645bd173e86d8b38b54dc0b
Charlisteketee <charlisteketee@gmail.com>
2024-02-05 15:13:05 -0800

    Not much done, switching branches

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ require('dotenv').config();
 const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
+const cookieParser = require("cookie-parser");
+
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.set('view engine', 'ejs');
+
+
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -25,6 +29,8 @@ app.use(
   })
 );
 app.use(express.static('public'));
+app.use(cookieParser()); // creates and populates req.cookies
+
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
